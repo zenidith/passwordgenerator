f2d9c4d101822ab9bd8920f2a2c0edea1aee1c74
K Sheenan <kaynesheenan+git@gmail.com>
2021-08-06 00:29:33 +0900

    four basic functions to do with charsets

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // Generator functions https://net-comber.com/charset.html
 
 function getRandomLower() {
-
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(String.fromCharCode(177));
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getSymbol() {
+    const symbols = '!@#$%^&*()_-+=?/>,<~`';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getSymbol());
